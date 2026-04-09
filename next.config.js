d3d7e5d23f4401b6bd2ba396412cb75bc4bb5971
martin xu <hxu51@usfca.edu>
2026-04-09 14:27:51 -0700

    Update GitHub Pages static build with correct asset paths

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/taxsite-demo',
+  assetPrefix: '/taxsite-demo',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
